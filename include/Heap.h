a8e2c68a38002d1a9c1cb98fa9dd31c39aa8644d
fmg2099 <fmg2099@example.com>
2025-06-17 17:41:43 -0600

    Arregla clase Heap

--- include/Heap.h
+++ include/Heap.h
@@ -3,20 +3,23 @@
 #include <vector>
 #include <iostream>
 //#include <>
+//macro to print pointers in hex format
+#define PRINT_POINTER(p) std::showbase << std::hex << reinterpret_cast<uintptr_t>(p) << std::dec
+
 template<class T>
 class Heap
 {
-	std::vector<HeapNode<T>> heap;
+    std::vector<HeapNode<T>> heap;
 
-	int parent(int i) { return (i - 1) / 2; }
-	int left(int i) { return 2 * i + 1; }
-	int right(int i) { return 2 * i + 2; }
+    int parent(int i) { return (i - 1) / 2; }
+    int left(int i) { return 2 * i + 1; }
+    int right(int i) { return 2 * i + 2; }
 
     void heapify_up(int i) {
-       //while (i > 0 && heap[i].GetValue() > heap[parent(i)].GetValue()) {
-            //std::swap(heap[i], heap[parent(i)]);
-            //i = parent(i);
-        //}
+        while (i > 0 && heap[parent(i)].GetValue() < heap[i].GetValue()) {
+            std::swap(heap[i], heap[parent(i)]);
+            i = parent(i);
+        }
     }
 
     void heapify_down(int i) {
@@ -24,7 +27,7 @@ class Heap
         int l = left(i);
         int r = right(i);
 
-        if (l < heap.size() && heap[l].GetValue() > heap[largest].GetValue()) largest = l; 
+        if (l < heap.size() && heap[l].GetValue() > heap[largest].GetValue()) largest = l;
         if (r < heap.size() && heap[r].GetValue() > heap[largest].GetValue()) largest = r;
 
         if (largest != i) {
@@ -35,13 +38,17 @@ class Heap
 
 
 
-public: 
+public:
     Heap() {
-        //heap = new std::vector<HeapNode<T> >();
+        //inicializar heap
+        heap.reserve(10);
+        std::cout << "Heap creado: " << heap.size() << std::endl;
     }
 
     void insert(T element, int val) {
-        HeapNode<T>* hn = new HeapNode<T>(&element, val);
+        HeapNode<T>* hn = new HeapNode<T>(element, val);
+        std::cout << "insertando " << PRINT_POINTER(hn) << std::endl;
+        std::cout << "data: " << PRINT_POINTER(hn->GetData()) << std::endl;
         heap.push_back(*hn);
         heapify_up(heap.size() - 1);
     }
@@ -53,24 +60,41 @@ public:
             std::cout << "heap esta vacía" << std::endl;
             return T();
         }
-        T* top = heap.at(0).GetData();
-        
+        const T* top = heap.at(0).GetData();
+
         heap[0] = heap.back();
         heap.pop_back();
         heapify_down(0);
         return *top;
     }
 
-    int peek() const {
+    const T* peek() const {
         if (heap.empty()) {
-            std::count<<"heap esta vacía" << std::endl;
             return nullptr;
         }
-        return heap.at(0);
+        return heap.at(0).GetData();
     }
 
     bool is_empty() const {
         return heap.empty();
+    }
+
+    //itera en todos los elementos del heap, para debuggear
+    void print() const {
+        std::cout << "Heap contiene: " << heap.size() << "elementos" << std::endl;
+        if (heap.empty())return;
+
+        std::cout << "|INDEX\t" << "|NODEDATA\t" << "|NODEVAL\t" << "|ADDRESS\t" << std::endl;
+        for (size_t i = 0; i < heap.size(); i++)
+        {
+            const T* data = heap[i].GetData();
+            std::cout
+                << i << "\t|"
+                << *data << "\t\t|"
+                << heap[i].GetValue() << "\t\t|"
+                << std::showbase << std::hex << reinterpret_cast<uintptr_t>(data) << std::dec
+                << std::endl;
+        }
     }
 };
 
